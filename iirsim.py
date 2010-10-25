a41f10e1033b6be9c0855791ef1b3e737a8ce934
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2010-10-25 20:38:12 +0000

    added iirsim.py

---
+++ iirsim.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt4 import QtCore, QtGui
+
+import src.iirsim_gui as iirsim_gui
+
+if __name__=='__main__':
+    app = QtGui.QApplication(sys.argv)
+    main = iirsim_gui.iirSimMainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
