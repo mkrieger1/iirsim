84accd466c0f5b8e033508d409d40290096fcafa
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2015-12-10 18:30:21 +0100

    add a function to convert a real number to a two's complement representation

--- iirsim/core.py
+++ iirsim/core.py
@@ -57,3 +57,25 @@ def _test_overflow(x, N):
     """
     limit = 1 << (N - 1)
     return (x < -limit) | (x >= limit)
+
+def from_real(x, N, M=0):
+    """
+    Map x from the real interval [-r, r) to the range of N-bit two's complement
+    numbers [-B, ..., B-1], rounding to the nearest possible value, where
+    r = 2 ** M (default: 1.0),
+    B = 2 ** (N - 1).
+
+    >>> [from_real(x, 8) for x in [0.7, 0.8, 0.9, 1.0, 1.1]]
+    [90, 102, 115, 127, 127]
+
+    >>> list(from_real(numpy.linspace(0.6, 1.1, 10), 7))
+    [38, 42, 46, 49, 53, 56, 60, 63, 63, 63]
+
+    >>> list(from_real(numpy.linspace(0.3, 0.6, 10), 8, -1))
+    [77, 85, 94, 102, 111, 119, 127, 127, 127, 127]
+
+    >>> list(from_real(numpy.linspace(-4.5, -3.0, 10), 8, 2))
+    [-128, -128, -128, -128, -123, -117, -112, -107, -101, -96]
+    """
+    y = x * (2 ** (N - 1 - M))
+    return _saturate(numpy.rint(y).astype(int), N)
