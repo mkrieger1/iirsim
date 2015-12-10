8e41504f10eefa1b8314e8868047e9525df30993
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2015-12-10 20:54:44 +0100

    add notes to the doctests and add one additional doctests

--- iirsim/core.py
+++ iirsim/core.py
@@ -69,16 +69,16 @@ def from_real(x, N, M=0):
     Input values outside [-r, r) will be mapped to integers that cannot be
     represented as an N-bit two's complement number.
 
-    >>> [from_real(x, 8) for x in [0.7, 0.8, 0.9, 1.0, 1.1]]
+    >>> [from_real(x, 8) for x in [0.7, 0.8, 0.9, 1.0, 1.1]] # 1.0 -> 128
     [90, 102, 115, 128, 141]
 
-    >>> list(from_real(numpy.linspace(0.6, 1.1, 10), 7))
+    >>> list(from_real(numpy.linspace(0.6, 1.1, 10), 7)) # 1.0 -> 64
     [38, 42, 46, 49, 53, 56, 60, 63, 67, 70]
 
-    >>> list(from_real(numpy.linspace(0.3, 0.6, 10), 8, -1))
+    >>> list(from_real(numpy.linspace(0.3, 0.6, 10), 8, -1)) # 0.5 -> 128
     [77, 85, 94, 102, 111, 119, 128, 137, 145, 154]
 
-    >>> list(from_real(numpy.linspace(-4.5, -3.0, 10), 8, 2))
+    >>> list(from_real(numpy.linspace(-4.5, -3.0, 10), 8, 2)) # 4.0 -> 128
     [-144, -139, -133, -128, -123, -117, -112, -107, -101, -96]
     """
     y = x * (2 ** (N - 1 - M))
@@ -91,10 +91,13 @@ def to_real(x, N, M=0):
     B = 2 ** (N - 1),
     r = 2 ** M (default: 1.0)
 
-    >>> [round(to_real(x, 8), 2) for x in [90, 100, 110, 120, 130]]
-    [0.7, 0.78, 0.86, 0.94, 1.02]
+    >>> [round(to_real(x, 8), 2) for x in [100, 110, 120, 130]] # 128 -> 1.0
+    [0.78, 0.86, 0.94, 1.02]
 
-    >>> [round(x, 2) for x in to_real(numpy.arange(48, 73, 4), 7)]
+    >>> [round(x, 2) for x in to_real(numpy.arange(48, 73, 4), 7)] # 64 -> 1.0
     [0.75, 0.81, 0.88, 0.94, 1.0, 1.06, 1.13]
+
+    >>> [round(x, 2) for x in to_real(numpy.arange(5, 10), 4, 1)] # 8 -> 2.0
+    [1.25, 1.5, 1.75, 2.0, 2.25]
     """
     return x * (2 ** (-(N - 1) + M))
